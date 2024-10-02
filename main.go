28e5f45cdd3c2f91c66256f44568c8b8804a6f67
javieracevedo <javier.acevedo233@gmail.com>
2024-10-01 21:01:53 -0400

    Clear screen, init shell, and print random quote on start.

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"gosh/config"
+	"math/rand"
+	"os"
+	"os/exec"
+)
+
+
+func exec_clear() {
+	clear_cmd := exec.Command("clear")
+	clear_cmd.Stdout = os.Stdout
+	clear_cmd.Run()
+}
+
+func printRandomQuote() {
+	quote_length := len(config.GlobalShellConfig.QuoteList)
+	if (quote_length > 0) {
+		randomIndex := rand.Intn(len(config.GlobalShellConfig.QuoteList))
+		randomItem := config.GlobalShellConfig.QuoteList[randomIndex]
+		fmt.Print(config.CYAN_COLOR, "Quote of the Day: \n\n", config.DEFAULT_COLOR)
+		fmt.Print(config.CYAN_COLOR, randomItem, "\n\n", config.DEFAULT_COLOR)
+	}
+}
+
+func main() {
+	exec_clear()
+
+	config.InitShellConfig()
+	
+	printRandomQuote()
+}
