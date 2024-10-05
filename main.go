cd52cd7d37d6395768884a93c85f5589f4565306
javieracevedo <javier.acevedo233@gmail.com>
2024-10-05 09:56:37 -0400

    Add exec package to place built-ins and syscalls wrappers funcs.

--- main.go
+++ main.go
@@ -1,37 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"gosh/config"
-	"log"
-	"math/rand"
-	"os"
-	"os/exec"
+	"gosh/exec"
 )
 
 
-func exec_clear() {
-    clear_cmd := exec.Command("clear")
-    clear_cmd.Stdout = os.Stdout
-    clear_cmd.Run()
-}
-
-func printRandomQuote() {
-    quote_length := len(config.GlobalShellConfig.QuoteList)
-    if (quote_length > 0) {
-        randomIndex := rand.Intn(len(config.GlobalShellConfig.QuoteList))
-        randomItem := config.GlobalShellConfig.QuoteList[randomIndex]
-        fmt.Print(config.CYAN_COLOR, "Quote of the Day: \n\n", config.DEFAULT_COLOR)
-        fmt.Print(config.CYAN_COLOR, randomItem, "\n\n", config.DEFAULT_COLOR)
-    }
-}
-
 func main() {
-    exec_clear()
+    exec.Clear()
 
-    _, initShellConfigErr := config.InitShellConfig("./gosh.rc")
-    if (initShellConfigErr != nil) {
-        log.Fatal(initShellConfigErr)
-    }
-    printRandomQuote()
+    config.InitShellConfig("./gosh.rc")
+
+    exec.DisplayRandomQuote()
 }
