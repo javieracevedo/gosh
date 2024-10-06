ab0bf8c1afe513cfd4c01c514db8e76adc757c1a
javieracevedo <javier.acevedo233@gmail.com>
2024-10-06 08:14:32 -0400

    Add util that checks if two slices are equal

--- utils/utils.go
+++ utils/utils.go
@@ -8,3 +8,17 @@ import (
 func GoshPrint(val any) {
     fmt.Print(config.GlobalShellConfig.PromptColor, val, "\n", config.DEFAULT_COLOR)
 }
+
+func SlicesEqual(a []string, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for idx := range a {
+		if a[idx] != b[idx] {
+			return false
+		}
+	}
+	
+	return true
+}
