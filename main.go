95efc23e9ff126e74e49a96da0242aff56d6e8e5
javieracevedo <javier.acevedo233@gmail.com>
2024-10-10 23:29:07 -0400

    Use local exec package to execute clear command, and use other exec mthods to execute commands from user input

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"gosh/config"
 	"gosh/exec"
+	"gosh/parser"
+	"log"
+	"os"
+	"syscall"
 )
 
 
 func main() {
-    exec.Clear()
-
     config.InitShellConfig("./gosh.rc")
+    
+    // Clear the screen
+    var ws syscall.WaitStatus
+    pid, _ := exec.ExecuteCommand([]string{"clear"})
+    syscall.Wait4(pid, &ws, 0, nil)
 
     exec.DisplayRandomQuote()
+
+    reader := bufio.NewReader(os.Stdin)
+
+    for {
+        fmt.Print(">> ")
+    
+        input, _ := reader.ReadString('\n')
+    
+        commands, err := parser.ParseCommandLine(input)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        exec.ExecuteCommandsAndWait(commands)
+    }
 }
