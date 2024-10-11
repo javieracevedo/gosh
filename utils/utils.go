a4b4d9c0c02d379946b82bacc052c22a11cf4772
javieracevedo <javier.acevedo233@gmail.com>
2024-10-11 14:00:01 -0400

    Handle space and tab characters in command line input parsing

--- utils/utils.go
+++ utils/utils.go
@@ -22,3 +22,25 @@ func SlicesEqual(a []string, b []string) bool {
     
     return true
 }
+
+func RemoveEmptyStrings(input []string) []string {
+    var result []string
+    for _, str := range input {
+        if str != "" {
+            result = append(result, str)
+        }
+    }
+    return result
+}
+
+func ConvertTabToSpaces(input string) string {
+    var result string
+    for _, char := range input {
+        if char == '\t' {
+            result += string(' ')
+        } else {
+			result = result + string(char)
+		}
+    }
+    return result
+}
