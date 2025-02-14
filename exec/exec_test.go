b06e587e7e6a412506e446f2f710233ceac021fc
javieracevedo <javier.acevedo233@gmail.com>
2025-02-14 08:50:51 -0400

    Implement builtin command cd

--- exec/exec_test.go
+++ exec/exec_test.go
@@ -1,10 +1,12 @@
 package exec_test
 
 import (
+	"errors"
 	"gosh/config"
 	"gosh/exec"
 	"gosh/utils"
 	"os"
+	"syscall"
 	"testing"
 )
 
@@ -45,26 +47,23 @@ func TestExistingCommandPathSet(t *testing.T) {
 func TestExistingCommandWithoutReadPermissions(t *testing.T) {
     initialShellConfig := config.ShellConfig{}
     configLines := []string{
-        "path=/home/panic/binaries/", 
+        "path=/home/panic/binaries/",
     }
     config.GlobalShellConfig = config.SetConfigValues(initialShellConfig, configLines[0])
-
     filename := "/home/panic/binaries/noreadtest"
     permissions := os.FileMode(0333)
     content := "#!/bin/bash \necho 'Hello, World!'"
-    utils.CreateFileWithPermissions(filename, permissions, content)
-
-    command := []string{"noreadtest"}
-    _, commandExecErr := exec.ExecuteCommand(command);
-
-
-    if (commandExecErr != nil) {
-        t.Errorf("exec.ExecuteCommand(%v) should not return an error when executing a command that exists with execution permission, but not read permissions.", command[0])
+	utils.CreateFileWithPermissions(filename, permissions, content)
+    
+	command := []string{"noreadtest"}
+    _, commandExecErr := exec.ExecuteCommand(command)
+    
+    if commandExecErr == nil {
+        t.Errorf("exec.ExecuteCommand(%v) should return an error when executing a shell script without read permissions.", command[0])
     }
-
-
-    t.Cleanup(func () {
-        removeErr := os.Remove(filename);
+    
+    t.Cleanup(func() {
+        removeErr := os.Remove(filename)
         if removeErr != nil {
             t.Errorf("Error deleting file: ")
         }
@@ -98,4 +97,88 @@ func TestExistingCommandWithoutExecutePermissions(t *testing.T) {
             t.Errorf("Error deleting file: ")
         }
     })
+}
+
+func TestBuiltInCd_EACCESS(t *testing.T) {
+    dirname := "/tmp/no_access_dir"
+    err := os.Mkdir(dirname, 0600)
+    if err != nil {
+        t.Fatalf("Failed to create test directory: %v", err)
+    }
+
+    cdError := exec.BuiltinCd(dirname)
+
+    if cdError == nil {
+        t.Errorf("BuiltinCd(%v) should return an error when changing to directory without execute permissions", dirname)
+    }
+    if !errors.Is(cdError, syscall.EACCES) {
+        t.Errorf("Expected EACCES error, got: %v", cdError)
+    }
+
+    t.Cleanup(func() {
+        os.Remove(dirname)
+    })
+}
+
+func TestBuiltInCd_ENAMETOOLONG(t *testing.T) {
+	dirname := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	
+	cdError := exec.BuiltinCd(dirname)
+
+	if cdError == nil {
+		t.Errorf("BuiltInCd(%v) should return an error when changing to directory with name longer than 255 characters.", dirname)
+	}
+
+	if !errors.Is(cdError, syscall.ENAMETOOLONG) {
+		t.Errorf("Expected ENAMETOOLONG error, got: %v", cdError)
+	}
+}
+
+func TestBuiltInCd_ENOENT(t *testing.T) {
+	dirname := "nonexistent_file"
+
+	cdError := exec.BuiltinCd(dirname)
+
+	if cdError == nil {
+		t.Errorf("BuiltInCd(%v) should return an error", dirname)
+	}
+
+	if !errors.Is(cdError, syscall.ENOENT) {
+		t.Errorf("Expected ENOENT error, got: %v", cdError)
+	}
+}
+
+func TestBuiltInCd_ENOTDIR(t *testing.T) {
+	fileName := "text.txt"
+	_, err := os.Create(fileName)
+	if (err != nil) {
+		t.Fatalf("Failed to create test file (text.txt): %v", err)
+	}
+
+	cdError := exec.BuiltinCd(fileName)
+	if (cdError == nil) {
+		t.Errorf("BuiltInCd(%v) should return an error when attempting to cd into a regular file", fileName)
+	}
+
+	if !errors.Is(cdError, syscall.ENOTDIR) {
+		t.Errorf("Expected ENOTDIR error, got: %v", cdError)
+	}
+
+	t.Cleanup(func() {
+        os.Remove(fileName)
+    })
+}
+
+func TestBuiltInCd_ELOOP(t *testing.T) {
+	link1 := "./test_files/link1"
+
+	cdError := exec.BuiltinCd(link1)
+
+	if (cdError == nil) {
+		t.Errorf("BuiltInCd(%v) should return an error when attempting to cd into circular symbolic links", link1)
+	}
+
+	if !errors.Is(cdError, syscall.ELOOP) {
+		t.Errorf("Expected EIO error, got: %v", cdError)
+	}
 }
